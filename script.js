dbdaa178027976b7ddf8d53baaad3d33a945100c
atifraza123 <101870376+atifraza123@users.noreply.github.com>
2023-05-25 21:33:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Get the button element
+// Get the button element
+const implementedBtn = document.getElementById('ImplementedBtn');
+
+// Add a click event listener to the button
+implementedBtn.addEventListener('click', function(Open) {
+  // Open a new page with the "Not implemented" message
+  window.open('implementedbtn.html', '_blank');
+});
